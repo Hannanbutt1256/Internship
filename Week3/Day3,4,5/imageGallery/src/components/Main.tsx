ca3edda518faf4f63dc00d126453df1f36ce8b45
Hannan Butt <hannanbutt1256@gmail.com>
2024-11-04 09:18:24 +0500

    some design changes

--- Week3/Day3,4,5/imageGallery/src/components/Main.tsx
+++ Week3/Day3,4,5/imageGallery/src/components/Main.tsx
@@ -10,13 +10,12 @@ import image8 from "../assets/nature (9).jpg";
 import image9 from "../assets/nature (10).jpg";
 import image10 from "../assets/nature (11).jpg";
 import image11 from "../assets/nature (12).jpg";
-import image12 from "../assets/nature (13).jpg";
 
 const Main = () => {
   return (
     <div className="p-3 px-7 text-charcoal font-mont">
       <h2 className="text-center font-semibold text-xl">Photo Gallery</h2>
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+      <div className="mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
         <div>
           <img src={image} className="rounded-2xl" />
         </div>
@@ -52,9 +51,6 @@ const Main = () => {
         </div>
         <div>
           <img src={image11} className="rounded-2xl" />
-        </div>
-        <div>
-          <img src={image12} className="rounded-2xl" />
         </div>
       </div>
     </div>
