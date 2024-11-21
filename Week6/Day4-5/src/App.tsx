522322ceb003e1b4b362b772a7322293f7b614a7
Hannan Butt <hannanbutt1256@gmail.com>
2024-11-21 16:32:01 +0500

    added vercel speed insights

--- Week6/Day4-5/src/App.tsx
+++ Week6/Day4-5/src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 function App() {
   return (
     <>
       <div className="font-poppins">
         <Navbar />
+        <SpeedInsights />
         <Outlet />
       </div>
     </>
